360f19119ceeba0ec98b1a54873fb2ecbf06b93b
marfa <hw2023use@gmail.com>
2024-08-11 05:04:10 +0300

    feat: create function decode(expr)

--- src/index.js
+++ src/index.js
@@ -39,6 +39,32 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arr = [];
+    let el = [];
+    let char;
+    let letter = [];
+
+    while (expr.length >= 9) {
+      el =  expr.split('', 10);
+      if ( el[0] === '*' ) arr.push( ' ' );
+
+      expr = expr.slice(10);
+
+      char = el.indexOf('1');
+      el.splice(0, [char] );
+
+      for ( i = 1; i <= el.length; i++ ) {
+        if ( i % 2 === 0 ) {
+          if ( el[i - 1] === '0' ) letter.push( '.' );
+          if ( el[i - 1] === '1' ) letter.push( '-' );
+        }
+      }
+
+      arr.push(MORSE_TABLE[letter.join('')]);
+      letter = [];
+    }
+
+    return arr.join('');
 }
 
 module.exports = {
